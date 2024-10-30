14f3f6909fcd952ea0476f36e431b5cbbbcf571c
rowa90ameen <159520497+rowa90ameen@users.noreply.github.com>
2024-10-30 09:35:28 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <section className="p-4">
+      <div className="container">
+        <h1 className="text-2xl font-bold">Home Page</h1>
+        <p className="w-1/2 font-medium py-2">
+          This page is about 6 different blogs.
+        </p>
+      </div>
+    </section>
+  );
+}
